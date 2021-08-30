62d6ee9df73152bb3637469916ce58725d450594
nikitsch <79355999+nikitsch@users.noreply.github.com>
2021-08-31 00:28:23 +0300

    init: index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 function eval() {
-    // Do not use eval!!!
     return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    expr = expr.replace(/ /g, '');
+    if (expr.indexOf('/0') !== -1)
+      throw new Error('TypeError: Division by zero.');
+    let bracket = 0;
+    expr.split('').forEach((v) => {
+      v === ')' ? bracket-- : false;
+      v === '(' ? bracket++ : false;
+    });
+    if (bracket !== 0)
+      throw new Error('ExpressionError: Brackets must be paired');
+    expr = new Function('return ' + expr);
+    return expr();
 }
 
 module.exports = {
